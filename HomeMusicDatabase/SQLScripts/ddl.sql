8d95d094edbaf8e6bc022be8ce049ea547ff6cc5
Magnus Lopez <ac148904@avcol.school.nz>
2025-03-13 10:35:00 +1300

    Changes have been made. I just realised that some of the tables were connected to an earlier devised name for one of my schemas, resulting in error. I also realised that I forgot to add 'TABLE' after create and just added the schema and table name, which I fixed.

--- HomeMusicDatabase/SQLScripts/ddl.sql
+++ HomeMusicDatabase/SQLScripts/ddl.sql
@@ -5,7 +5,7 @@ GO
 CREATE Schema Properties;
 GO
 
-CREATE Contents.tblAlbums(
+CREATE TABLE Contents.tblAlbums (
 	albumID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
 	albumName VARCHAR(255) NOT NULL,
 	genreName VARCHAR(255) NOT NULL,
@@ -13,32 +13,32 @@ CREATE Contents.tblAlbums(
 	FOREIGN KEY (formatID) REFERENCES Properties.tblFormat (formatID) 
 );
 
-CREATE Contents.tblArtists(
+CREATE TABLE Contents.tblArtists (
 	artistID INT IDENTITY(1,1) NOT NULL,
 	artistName VARCHAR(255) NOT NULL,
 	FOREIGN KEY (albumID) REFERENCES Contents.tblAlbums (albumID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE Properties.tblBands(
+CREATE TABLE Contents.tblBands (
 	bandID INT IDENTITY(1,1) NOT NULL,
 	bandName VARCHAR(255) NOT NULL,
 	FOREIGN KEY (albumID) REFERENCES Contents.tblAlbums (albumID) ON DELETE CASCADE ON UPDATE CASCADE	
 );
 
-CREATE Inventory.tblFormat(
+CREATE TABLE Properties.tblFormat (
 	formatID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
 	formatName VARCHAR(10) NOT NULL,
 	FOREIGN KEY (roomID) REFERENCES Properties.tblStorageInfo (roomID),
 	FOREIGN KEY (albumID) REFERENCES Contents.tblAlbums (albumID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE Inventory.tblStorageInfo(
+CREATE TABLE Properties.tblStorageInfo (
 	roomID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
 	roomName VARCHAR(20) NOT NULL,
 );
 
-CREATE Inventory.tblShelfInfo(
-	shelfTag CHAR NOT NULL,
+CREATE TABLE Properties.tblShelfInfo (
+	shelfTag CHAR(1) NOT NULL,
 	shelfRow INT IDENTITY(1,1) NOT NULL,
 	FOREIGN KEY (roomID) REFERENCES Properties.tblStorageInfo (roomID)
 );
